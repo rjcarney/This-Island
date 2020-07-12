f58cb458c2496f3f1a4ab5a1d0508fec1cbd24f1
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-07-12 19:48:16 -0400

    Player Movement
    Player moves from WASD keyboard input. Player jumps when spacebar is pressed and falls due to gravity.

--- This Island/Assets/Scripts/PlayerMovement.cs
+++ This Island/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
+    public Transform groundCheck;
+    public float groundDistance =0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
     private CharacterController controller;
+    private Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +25,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+        // WASD Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Fall From Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }

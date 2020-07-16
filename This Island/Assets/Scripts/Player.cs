4f77f4200f54d8616cbc929e7e8577f8558d949b
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-07-16 01:49:26 -0400

    Player Survival Attributes
    Health, hunger, thisrt, stamina, and temperature.

---
+++ This Island/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float maxHealth, maxHunger, maxThirst, maxStamina, minTemperature, maxTemperature;
+    public float hungerIncreaseRate, thirstIncreaseRate, staminaDecreaseRate, staminaRegenRate;
+    public float health, hunger, thirst, stamina, temperature;
+    private bool dead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dead = false;
+        health = maxHealth;
+        stamina = maxStamina;
+        temperature = 98.6f;
+        hunger = 0;
+        thirst = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead) {
+            thirst += thirstIncreaseRate * Time.deltaTime;
+            hunger += hungerIncreaseRate * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            if(stamina > 0) {
+                stamina -= staminaDecreaseRate * Time.deltaTime;
+                this.gameObject.GetComponent<PlayerMovement>().speed = 20;
+            } else {
+                print("Out of stamina, you can't sprint");
+                this.gameObject.GetComponent<PlayerMovement>().speed = 12;
+            }
+        } else {
+            this.gameObject.GetComponent<PlayerMovement>().speed = 12;
+            if (stamina < maxStamina) {
+                stamina += staminaRegenRate * Time.deltaTime;
+            }
+        }
+
+        if(health <= 0 || hunger >= maxHunger || thirst >= maxThirst
+            || temperature >= maxTemperature || temperature <= minTemperature)
+            Die();
+    }
+
+    public void Die() {
+        dead = true;
+        print("You have died");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Food") {
+
+        }
+
+        if (other.tag == "Water") {
+
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+
+    }
+}
